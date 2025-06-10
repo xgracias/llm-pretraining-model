6a439e6371962097de783481550f9c3aca02a424
xgracias <49041460+xgracias@users.noreply.github.com>
2025-06-09 17:35:53 -0700

    add llm architecture

--- src/main.py
+++ src/main.py
@@ -1,27 +1,94 @@
-import os
-import re
+from importlib.metadata import version
 
-file_path = os.path.join(os.path.dirname(__file__), "", "the-verdict.txt")
-with open(file_path, "r", encoding="utf-8") as file:
-    verdict = file.read()
+print("torch version:", version("torch"))
+print("tiktoken version:", version("tiktoken"))
 
-print("Total number of characters in the verdict:", len(verdict))
-print("First 100 characters of the verdict:", verdict[:100])
+GPT_CONFIG_124M = {
+    "vocab_size": 50257,    # Vocabulary size
+    "context_length": 1024, # Context length
+    "emb_dim": 768,         # Embedding dimension
+    "n_heads": 12,          # Number of attention heads
+    "n_layers": 12,         # Number of layers
+    "drop_rate": 0.0,       # Dropout rate
+    "qkv_bias": False       # Query-Key-Value bias
+}
 
-# split on whitespaces and punctuation
-preprocessed = re.split(r'([,.:;?_!"()\']|--|\s)', verdict)
-preprocessed = [item for item in preprocessed if item]
-print("\nFirst 38 tokens:", preprocessed[:38])  # print first 38 tokens
-print("Total number of tokens:", len(preprocessed))
+import torch.nn as nn
+from supplementary import TransformerBlock, LayerNorm
 
-# vocabulary contains the unique words in the input text
-all_words = sorted(set(preprocessed))
-vocab_size = len(all_words)
-print("\nVocabulary size:", vocab_size)
-vocab = {token:integer for integer, token in enumerate(all_words)}
+class GPTModel(nn.Module):
+    def __init__(self, cfg):
+        super().__init__()
+        self.tok_emb = nn.Embedding(cfg["vocab_size"], cfg["emb_dim"])
+        self.pos_emb = nn.Embedding(cfg["context_length"], cfg["emb_dim"])
+        self.drop_emb = nn.Dropout(cfg["drop_rate"])
+        
+        self.trf_blocks = nn.Sequential(
+            *[TransformerBlock(cfg) for _ in range(cfg["n_layers"])])
+        
+        self.final_norm = LayerNorm(cfg["emb_dim"])
+        self.out_head = nn.Linear(
+            cfg["emb_dim"], cfg["vocab_size"], bias=False
+        )
 
-# print first 50 items in the vocabulary
-for i, item in enumerate(vocab.items()):
-    print(item)
-    if (i >= 50):
-        break
+    def forward(self, in_idx):
+        batch_size, seq_len = in_idx.shape
+        tok_embeds = self.tok_emb(in_idx)
+        pos_embeds = self.pos_emb(torch.arange(seq_len, device=in_idx.device))
+        x = tok_embeds + pos_embeds  # Shape [batch_size, num_tokens, emb_size]
+        x = self.drop_emb(x)
+        x = self.trf_blocks(x)
+        x = self.final_norm(x)
+        logits = self.out_head(x)
+        return logits
+
+import torch
+import tiktoken
+
+tokenizer = tiktoken.get_encoding("gpt2")
+
+batch = []
+
+txt1 = "Every effort moves you"
+txt2 = "Every day holds a"
+
+batch.append(torch.tensor(tokenizer.encode(txt1)))
+batch.append(torch.tensor(tokenizer.encode(txt2)))
+batch = torch.stack(batch, dim=0)
+print(batch)
+
+torch.manual_seed(123)
+model = GPTModel(GPT_CONFIG_124M)
+
+out = model(batch)
+print("Input batch:\n", batch)
+print("\nOutput shape:", out.shape)
+print(out)
+
+def generate_text_simple(model, idx, max_new_tokens, context_size):
+    # idx is (batch, n_tokens) array of indices in the current context
+    for _ in range(max_new_tokens):
+        
+        # Crop current context if it exceeds the supported context size
+        # E.g., if LLM supports only 5 tokens, and the context size is 10
+        # then only the last 5 tokens are used as context
+        idx_cond = idx[:, -context_size:]
+        
+        # Get the predictions
+        with torch.no_grad():
+            logits = model(idx_cond)
+        
+        # Focus only on the last time step
+        # (batch, n_tokens, vocab_size) becomes (batch, vocab_size)
+        logits = logits[:, -1, :]  
+
+        # Apply softmax to get probabilities
+        probas = torch.softmax(logits, dim=-1)  # (batch, vocab_size)
+
+        # Get the idx of the vocab entry with the highest probability value
+        idx_next = torch.argmax(probas, dim=-1, keepdim=True)  # (batch, 1)
+
+        # Append sampled index to the running sequence
+        idx = torch.cat((idx, idx_next), dim=1)  # (batch, n_tokens+1)
+
+    return idx
--- src/tokenizer.py
+++
@@ -1,2 +0,0 @@
-class SimpleTokenizerV1:
-    def __init__(self, vocab):
